b6078ed4b48b7d04f03150cacc554774409c2c9e
ALP ERTEN <alperten95@gmail.com>
2021-01-31 19:16:45 +0300

    Second Commit

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom'
+
+const About = () => {
+    return (
+        <div className="about">
+        <br/>
+            <h4>Version 1.0.0</h4>
+            <Link to="/">Go Back</Link>
+            <br/><br/>
+        </div>
+    )
+}
+
+export default About
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import PropTypes from 'prop-types'
+import {useLocation} from 'react-router-dom'
 import Button from './Button'
 
-const Header = ({title}) => {
-    const onClick = () =>{
-        console.log("clicked")
-    }
+const Header = ({title,onAdd,showAdd}) => {
+    const location = useLocation()
+
     return (
         <header  className="header">
             <h1>{title}</h1>
-            <Button color='black' text="Add" onClick={onClick}></Button>
+            {location.pathname === '/' && (<Button color='black' text={showAdd ? "Close" : "Add"} onClick={onAdd}></Button>)}
         </header>
     )
 }
